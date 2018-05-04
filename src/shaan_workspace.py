ceac5c8f829f6286841cdcc7ac60f96f02a24d1c
Jebbit Admin <admin@jebbits-mbp.bc.edu>
2018-05-04 14:58:53 -0400

    Adds code to construct world and corpus

--- src/shaan_workspace.py
+++ src/shaan_workspace.py
@@ -1,6 +1,39 @@
 from pprint import pprint
-from world import world
+from world import world, corpus
 
 pprint(world.words_key)
-print world.numWords
+print world.num_words
 print world.words
+print world.words_freq
+
+pprint(world.objects_key)
+print world.num_objects
+print world.objects
+print world.objects_freq
+
+for i in range(len(world.objects_freq)):
+    print i, '\t', world.objects_key[i], '\t', world.objects_freq[i]
+
+
+# Testing data
+#This is still pseudocode!
+#
+# words = range(3)
+# words_key = {'ball': 0, 'bat': 1, 'ballie': 2}
+# numWords = 3
+#
+# objects = range(4)
+# objects_key = {
+#     'ball': 0,
+#     'toy': 1,
+#     'face': 2,
+#     'bat': 3
+# }
+# num_objects = 23
+#
+# words_freq = [1, 1]
+# # objects_freq = [5, 18, 14 134 113 15 21 67 75 19 5 15 102 43 25 11 15 28 140 199 175 12 15]
+# # mis: []
+#
+# script = "look at this ball now look at this bat" # Every word that's said
+# # [ball toy face] [ball toy bat]
--- src/world.py
+++ src/world.py
@@ -1,60 +1,64 @@
+from situation import Situation
+
 class WorldObj:
     def __initKey(self, filename):
+        # Input: filename of the word/object file
+        # Output: int index->string word key
+        freqs = []
         key = {}
         f = open(filename, 'r')
-        added = set()
+        added = []
         i = 0
         for line in f.readlines():
             for word in line.replace('\t','').replace('\n','').split(' '):
+                if word == '': continue
                 if word not in added:
                     key[i] = word
-                    added.add(word)
+                    added.append(word)
+                    freqs.append(1)
                     i += 1
-        return key
+                else:
+                    freqs[added.index(word)] += 1
+        f.close()
+        return key, freqs
 
-    def __init__(self):
-        wordFile = "words.txt"
-        objectFile = "objects.txt"
-        self.words_key = self.__initKey(wordFile)
-        self.numWords = len(self.words_key)
-        self.words = range(self.numWords)
+    def __init__(self, wordFile, objectFile):
+        self.words_key, self.words_freq = self.__initKey(wordFile)
+        self.num_words = len(self.words_key)
+        self.words = range(self.num_words)
 
-        self.objects_key = self.__initKey(objectFile)
-        self.numObjects = len(self.objects_key)
-        self.objects = range(self.numObjects)
-        #
-        # self.words_freq = []
-        # self.objects_freq = [5 18 14 134 113 15 21 67 75 19 5 15 102 43 25 11 15 28 140 199 175 12 15]
+        self.objects_key, self.objects_freq = self.__initKey(objectFile)
+        self.num_objects = len(self.objects_key)
+        self.objects = range(self.num_objects)
+
         # self.mis: []
-        #
-        # self.alpha = 0.1
-        # self.gamma = 0.1
-        # self.kappa = 0
-        #
-        # self.script = "" # Every word that's said
-        #
 
-world = WorldObj()
+def processLine(s):
+    return s.replace('\t', '').replace('\n', '')
 
-# Testing data
-#This is still pseudocode!
-#
-# words = range(3)
-# words_key = {'ball': 0, 'bat': 1, 'ballie': 2}
-# numWords = 3
-#
-# objects = range(4)
-# objects_key = {
-#     'ball': 0,
-#     'toy': 1,
-#     'face': 2,
-#     'bat': 3
-# }
-# num_objects = 23
-#
-# words_freq = [1, 1]
-# # objects_freq = [5, 18, 14 134 113 15 21 67 75 19 5 15 102 43 25 11 15 28 140 199 175 12 15]
-# # mis: []
-#
-# script = "look at this ball now look at this bat" # Every word that's said
-# # [ball toy face] [ball toy bat]
+def makeCorpus(wordFile, objectFile):
+    # Input: filenames of word and object files
+    # Output: Corpus as list of Situations
+    corpus = []
+    wf = open(wordFile, 'r')
+    of = open(objectFile, 'r')
+
+    wl = wf.readline()
+    ol = of.readline()
+    while(len(wl)!=0 and len(ol)!=0):
+        wl = processLine(wl)
+        ol = processLine(ol)
+
+        words = [x for x in wl.split(' ') if x!='']
+        objects = [x for x in ol.split(' ') if x!='']
+        corpus.append(Situation(words, objects))
+
+        wl = wf.readline()
+        ol = of.readline()
+    return corpus
+
+wordFile = "words.txt"
+objectFile = "objects.txt"
+
+world = WorldObj(wordFile, objectFile)
+corpus = makeCorpus(wordFile, objectFile)
